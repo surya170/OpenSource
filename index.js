1a8fe9131a515759e3fe9593fc66111b8f9afac7
surya9014 <suryanarayana13271@gmail.com>
2024-10-17 13:55:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express(); 
+const bodyParser = require("body-parser")
+//middleware
+app.use(bodyParser.json())
+const port = process.env.PORT || 3000;
+
+
+app.get("/",function (req,res)  {
+    res.send('<b>Hello World!</b>')
+})
+
+app.get("/route-handler",function (req,res) {
+    //headers,body,query parameters 
+    // do machine learning model 
+    res.json({
+        name:"Surya",
+        age:21
+    })
+})
+
+app.post("/conversation",(req,res) => {
+    console.log(req.headers['authorization']);
+    console.log(req.body)
+    res.send({
+        msg: "2+2 = 4"
+    })
+})
+
+app.listen(port, function() {
+    console.log(`Example app listening on port ${port}`)
+})
